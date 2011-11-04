69ab73b8142b10ff6fd1a1061aae18fd5dce8eec
Callum Macrae <callum@lynxphp.com>
2011-11-04 21:59:51 +0000

    Added .removeHandlers.

--- events.bacon.js
+++ events.bacon.js
@@ -1,12 +1,22 @@
+bacon._eventData = [];
+
 bacon.html.on = function(event, callback) {
 	this.each(function() {
 		if (this.addEventListener) {
-			this.addEventListener(event, function(e) {
+			var handler = function(e) {
 				if (callback.call(this, e) === false) {
 					e.stopPropagation();
 					e.preventDefault();
 				}
-			});
+			};
+			handler.callback = callback;
+			this.addEventListener(event, handler);
+			
+			if (!this.dataset.baconId) {
+				this.dataset.baconId = bacon._eventData.push([['click', handler]]) - 1;
+			} else {
+				bacon._eventData[this.dataset.baconId].push(['click', handler]);
+			}
 		} else {
 			// IE
 		}
@@ -26,6 +36,24 @@ bacon.html.trigger = function(event, callback) {
 			}
 		} else {
 			// IE
+		}
+	});
+	return this;
+}
+
+bacon.html.removeHandlers = bacon.html.off = function(event, callback) {
+	this.each(function() {
+		if (this.dataset.baconId) {
+			var data = bacon._eventData[this.dataset.baconId];
+			for (var i = 0; i < data.length; i++) {
+				if (data[i][0] === event && (typeof callback === 'undefined' || data[i][1].callback === callback)) {
+					if (this.removeEventListener) {
+						this.removeEventListener(data[i][0], data[i][1]);
+					} else {
+						// IE
+					}
+				}
+			}
 		}
 	});
 	return this;
