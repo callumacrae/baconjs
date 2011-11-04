d01810a7b1bdc4e1717d6bc158015db5aef05117
Callum Macrae <callum@lynxphp.com>
2011-11-04 20:05:17 +0000

    Added a limit argument to the select function.

--- core.bacon.js
+++ core.bacon.js
@@ -1,21 +1,21 @@
 function BaconObj() {};
 
-var $, bacon = $ = function(selector) {
+var $, bacon = $ = function(selector, limit) {
 	var elements = new BaconObj();
 	if (typeof selector === 'object') {
 		elements.elements = (selector instanceof Array) ? selector : [selector];
 	} else {
 		elements.elements = [document];
-		elements.select(selector);
+		elements.select(selector, limit);
 	}
 	return (elements.elements === null ) ? null : elements;
 };
 BaconObj.prototype = bacon.html = {};
 
-bacon.html.select = function(selector) {
-	for (var els, elements = [], i = 0, j; i < this.elements.length; i++) {
+bacon.html.select = function(selector, limit) {
+	for (var els, elements = [], i = 0, j; i < this.elements.length && (typeof limit === 'undefined' || elements.length < limit); i++) {
 		els = this.elements[i].querySelectorAll(selector);
-		for (j = 0; j < els.length; j++) {
+		for (j = 0; j < els.length && (typeof limit === 'undefined' || elements.length < limit); j++) {
 			elements.push(els[j]);
 		}
 	}
