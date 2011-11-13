94969a04b727e7f9ab7f17faf430088324dd7f7a
Callum Macrae <callum@lynxphp.com>
2011-11-13 14:53:40 +0000

    Renamed the aJSON test functions to JSON.

--- bacon/bacon.js
+++ bacon/bacon.js
@@ -777,14 +777,14 @@ bacon.enableArrayFeatures = function() {
  *                                 OLD BROWSERS
  ****************************************************************************/
 
-if (typeof aJSON === 'undefined') {
-	aJSON = {};
+if (typeof JSON === 'undefined') {
+	JSON = {};
 	
-	aJSON.parse = function(text) {
+	JSON.parse = function(text) {
 		return eval('(' + text + ')');
 	};
 	
-	aJSON.stringify = function(value) {
+	JSON.stringify = function(value) {
 		 var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
 			escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
 			meta = {
@@ -798,8 +798,8 @@ if (typeof aJSON === 'undefined') {
 			};
 		
 		function str(value) {
-			if (value.toaJSON) {
-				return '"' + value.toaJSON() + '"';
+			if (value.toJSON) {
+				return '"' + value.toJSON() + '"';
 			}
 			if (typeof value === 'string') {
 				if (escapable.test(value.valueOf())) {
@@ -845,8 +845,8 @@ if (typeof aJSON === 'undefined') {
 		return str(value);
 	};
 
-	if (typeof Date.prototype.toaJSON !== 'function') {
-		Date.prototype.toaJSON = function(key) {
+	if (typeof Date.prototype.toJSON !== 'function') {
+		Date.prototype.toJSON = function(key) {
 			function f(n) {
 				return n < 10 ? '0' + n : n;
 			}
